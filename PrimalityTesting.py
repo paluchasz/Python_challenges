17ee416232ad378bb0a03a7be08e32cda78ec4cd
paluchasz <paluchasz@googlemail.com>
2018-10-10 15:46:53 +0100

    Update PrimalityTesting.py

--- PrimalityTesting.py
+++ PrimalityTesting.py
@@ -1,4 +1,7 @@
-#Primality tests
+#Primality tests. To check if n is prime we need to check that it doesn't divide integers less than n.
+#This would be a linear O(n) time complexity. However, we can do better by noting that sqrt(n)*sqrt(n) = n.
+#Because of this if we look at integer higher than sqrt(n) then if it divides n it's other factor will have to be
+#an integer smaller than sqrt(n) but we would have already considered this integer. Hence this algorithm is O(n^1/2) 
 import math
 
 def isPrimeV1(n):
