62bddec545191a5b48556dca7667028b94177199
paluchasz <paluchasz@googlemail.com>
2020-10-21 18:25:57 +0100

    Sort files in directories

--- Sorting_algorithms/Insertion_sort.py
+++ Sorting_algorithms/Insertion_sort.py
@@ -4,12 +4,12 @@ ordered list on the left. This is done by looping from the right and moving the 
 the right to make space for the new element.'''
 
 
-def InsertionSort(arr):
-    
+def insertionSort(arr):
+
 #start at i=1 and our a[0] is a sorted sublist since it is just one element
     for i in range(1, len(arr)):
         temporary = arr[i]
-        #print(arr) to see how it works
+        print(arr) #to see how it works
 
         while i>0 and arr[i-1] > temporary: #this was the crucial step to have i>0 too to avoid IndexError
             arr[i] = arr[i-1]               #and not some try/except statements..
@@ -21,10 +21,10 @@ def InsertionSort(arr):
 
 def main():
     arr = [4,2,8,3,1,5,6]
-    result = InsertionSort(arr)
+    result = insertionSort(arr)
     print(result)
     arr2 = [-1,8,-5,0,3,4,1,2]
-    result2 = InsertionSort(arr2)
+    result2 = insertionSort(arr2)
     print(result2)
 
 main()
